7f5690942f19e9158ae31f5fc3ead6b420a15214
Sr-Vinicius <rodriguesviniciusfreitas@gmail.com>
2023-07-06 22:08:54 -0300

    readme update

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# pca9685_raspberry
+# A PCA9685 C++ Library for Embedded Linux 
+
+<img src="https://img.shields.io/badge/version-v1.0-red"> <img src="https://img.shields.io/badge/license-LGPL 2.1-red">
+
 C++ library for the multi-channel PWM generator PCA9685. Originally designed to be an I²C-bus LED controller IC, but it's also used to control servos.
+
+## Supported Features
+* Independent PWM signal generation on all 16 channels;
+* Multiple PCA9685 programming simultaneously with the ALLCALL address (configurable);
+* PWM signal frequency configuration;
+* PCA9685 sleep function;
+* PCA9685 restart function;
+* Generating the same PWM signal on all channels via *all_channel* methods;
+* Full device configuration including: pin topology, pin inverting, output change;
+
+## Building and executing the test code
+Clone the repository to your Linux device:
+```bash
+git clone https://github.com/Sr-Vinicius/pca9685_raspberry.git
+```
+
+Go to the examples folder:
+```bash
+cd pca9685_raspberry/examples
+```
+
+And then, just run the make command:
+```bash
+make
+```
+
+Finally, run the test:
+```bash
+./i2c_test
+```
+
+# Next features
+
+* Enable external clock source to the PCA9685;
+
+By now, the chip can only work with it's internal oscilator clock source. It's possible to supply external clock to the PCA9685, configuring the appropriate registers and generating a clock signal with the Linux device or with dedicated hardware.
+
+## License
+This library is licensed under the **GNU LGPL v2.1**. 
+
+## Acknowledgements
+This library is based on the C++ I2C device template developed by [Derek Molloy](https://github.com/derekmolloy), and available [here](https://github.com/derekmolloy/exploringrpi).
--- examples/i2c_test.cpp
+++ examples/i2c_test.cpp
@@ -6,17 +6,11 @@ using namespace exploringRPi;
 
 int main(int argc, char *argv[]){
   std::cout << "\n===================================================" << std::endl;
-  std::cout << "TESTE DE TEMPLATE I2C para PCA9685\n\n" << std::endl;
+  std::cout << "PCA9685 I2C test:\n\n" << std::endl;
 
-  // pca9685 driver1(1, 0x40);
-  //pca9685 driver2(1, 0x43);
-  pca9685 servo_driver(1, 0x43);
-  
-  // driver1.enable_allcall_response();
-  // driver2.enable_allcall_response();
+  pca9685 servo_driver(1, 0x43); /*bus 1, address 0x43*/
 
-  servo_driver.set_output_drive(pca9685::OPEN_DRAIN);
-  servo_driver.set_output_drive(pca9685::TOTEM_POLE);
+  servo_driver.set_output_drive(pca9685::TOTEM_POLE); // or OPEN_DRAIN, as you need
   servo_driver.set_output_inverting(false);
   servo_driver.set_output_enable_pin(26);
   servo_driver.set_output_enable_value(LOW);
@@ -25,16 +19,13 @@ int main(int argc, char *argv[]){
   servo_driver.enable_channel(pca9685::CH00);
   servo_driver.set_pwm_duty_cycle(pca9685::CH00, 1000);
 
-  //servo_driver.enable_all_channel();
-  //servo_driver.set_all_pwm_duty_cycle(1200);
-
-  /*
-  std::cout << "output chaange trigger test:" << std::endl;
-  servo_driver.set_output_change(pca9685::ON_ACK);
-  servo_driver.set_output_change(pca9685::ON_STOP);
-
-  std::cout << "inverted logic output test:" << std::endl;
-  servo_driver.set_output_inverting(true);
-  servo_driver.set_output_inverting(false);
-  */
+  std::cout << "Test complete!" << std::endl;
+  std::cout << "Verify modified registers with the comand:\n"
+               "$ i2cget -y 1 0x43 <register_addr>\n\n"
+               "Here is a list of the modified registers:\n"
+               "MODE1: 0x00\nMODE2: 0x01\nPRE_SCALE: 0xFE\n"
+               "LED0_ON_L: 0x06\nLED_ON_H: 0x07\n"
+               "LED0_OFF_L: 0x08\nLED_OFF_H: 0x09\n\n"
+               "You can also check the PWM outputs using"
+               "an osciloscope, a logic analizer or a LED."<<std::endl;
 }
